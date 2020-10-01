2bf5c593532f48db9fe86b693e98a3e5ce0f733b
david <david.barkhuizen@gmail.com>
2020-10-01 19:32:50 +0200

    ssh pvt key for git clone supplied via docker-swarm secret

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,10 @@ COPY ./node/package*.json ./
 RUN npm install
 COPY ./node/src ./src
 
+RUN mkdir -p /root/.ssh
+RUN chmod 0700 /root/.ssh
+RUN ssh-keyscan github.com > /root/.ssh/known_hosts
+
 EXPOSE 8080
 
 CMD [ "node", "src/server.js" ]
--- node/src/server.js
+++ node/src/server.js
@@ -1,11 +1,41 @@
 const express = require('express');
+const { exec } = require('child_process');
+const fs = require('fs');
+const path = require('path');
+
 const app = express();
 const port = 8080;
 
+const fetchCode = () => { 
+
+    const repo = 'github.com:davidbarkhuizen/indrajala-fluid-client.git';
+    //const checkoutPath = '/tmp/checkout'
+
+    console.log('fetching');
+
+    const executing = exec(`ssh-agent bash -c 'ssh-add /run/secrets/id_rsa; git clone git@${repo}'`, function(err, stdout, stderr) {
+        if (err) {
+            console.log('error')
+            console.log(stderr)
+        }
+        console.log('normal termination');
+        console.log(stdout);
+    });
+
+    executing.on('exit', (exitCode) => {
+
+        console.log(`done, exit code ${exitCode}`);
+
+    })
+}
+
 app.get('/', (req, res) => {
     res.send('indrajala build-machine')
+
+    fetchCode();
 });
 
 app.listen(port, () => {
+
     console.log(`android build-machine listening on port ${port}`)
 });

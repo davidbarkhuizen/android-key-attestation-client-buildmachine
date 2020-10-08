e15749abb92e8cf00dece5cd67cfe7f05ef0be3d
david <david.barkhuizen@gmail.com>
2020-10-08 16:10:11 +0200

    changed config to single json object

--- node/src/server.js
+++ node/src/server.js
@@ -25,74 +25,51 @@ app.post('/configure', async (req, res) => {
     console.log('*'.repeat(40));
     console.log('CONFIGURE');
 
-    const repoURL = req.body.repoURL;
-    await storage.setItem('repoURL', repoURL);
-    console.log('repoURL', repoURL);
+    const config = req.body;
 
-    const buildCommand = req.body.buildCommand;
-    await storage.setItem('buildCommand', buildCommand);
-    console.log('buildCommand', buildCommand);
-
-    const buildCommandPath = req.body.buildCommandPath;
-    await storage.setItem('buildCommandPath', buildCommandPath);
-    console.log('buildCommandPath', buildCommandPath);
-
-    const buildArtefactsFolderPath = req.body.buildArtefactsFolderPath;
-    await storage.setItem('buildArtefactsFolderPath', buildArtefactsFolderPath);
-    console.log('buildArtefactsFolderPath', buildArtefactsFolderPath);
-
-    const publishHost = req.body.publishHost;
-    await storage.setItem('publishHost', publishHost);
-    console.log('publishHost', publishHost);
-
-    const publishUser = req.body.publishUser;
-    await storage.setItem('publishUser', publishUser);
-    console.log('publishUser', publishUser);
-
-    const publishPath = req.body.publishPath;
-    await storage.setItem('publishPath', publishPath);
-    console.log('publishPath', publishPath);
+    await storage.setItem('config', config);
+    console.log(config);
 
     res.status(200);
     res.send('configured.');
 });
 
 app.post('/clone', async (req, res) => {
-
-    const repoURL = await storage.getItem('repoURL');
+    const config = await storage.getItem('config');
 
     await cleanLocation(CHECKOUT_PATH, 'source checkout');
-    await clone(repoURL, CHECKOUT_PATH);
+    await clone(config.repo.url, CHECKOUT_PATH);
 
     res.status(200);
-    res.send(`cloned ${repoURL}`);
+    res.send(`cloned from ${config.repo.url}`);
 });
 
 app.post('/build', async (req, res) => {
+    const config = await storage.getItem('config');
 
     console.log('*'.repeat(40));
     console.log('BUILD...');
 
-    const buildCommand = await storage.getItem('buildCommand');
-    const buildCommandPath = await storage.getItem('buildCommandPath');
-
-    const { commit, stdout_stderr } = await build(CHECKOUT_PATH, buildCommand, buildCommandPath, buildCommandPath);
+    const { commit, stdout_stderr } = await build(CHECKOUT_PATH, config.build.command, config.build.cwd);
 
     res.status(200);
     res.send(`build:\n${stdout_stderr}`);
 });
 
 app.post('/publish', async (req, res) => {
+    const config = await storage.getItem('config');
 
-    const buildArtefactsFolderPath = await storage.getItem('buildArtefactsFolderPath');
-    const host = await storage.getItem('publishHost');
-    const user = await storage.getItem('publishUser');
-    const path = await storage.getItem('publishPath');
-
-    const artefacts = await publish(host, user, PRIVATE_KEY_PATH, CHECKOUT_PATH, buildArtefactsFolderPath, path);
+    const artefacts = await publish(
+        config.publish.host, 
+        config.publish.user, 
+        PRIVATE_KEY_PATH, 
+        CHECKOUT_PATH, 
+        config.build.artefactsPath, 
+        config.publish.path
+    );
 
     res.status(200);
-    res.send(`published ${artefacts.length} artefacts to ${host}:${path}\n${artefacts.join('\n')}`);
+    res.send(`published ${artefacts.length} artefacts to ${config.publish.host}:${config.publish.path}\n${artefacts.join('\n')}`);
 });
 
 // entrypoint
